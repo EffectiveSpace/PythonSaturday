776fe8a9dbaf54d3d7da1ec0f50558997b011d61
Alex <acmarkov7@gmail.com>
2023-03-12 09:23:10 +0300

    продолжаем разрабатывать игру

--- GAMES/ping-pong/game.py
+++ GAMES/ping-pong/game.py
@@ -1,13 +1,22 @@
 import arcade
 
+SCREEN_WIDTH = 600
+SCREEN_HEIGHT = 600
+IMAGE_BALL = 'мячик.png'
+BALL_SCALE = 0.05
+SPEED_X = 5
+SPEED_Y = 3
+
 class GameWindow(arcade.Window):
     def __init__(self, width, height, title):
         super().__init__(width, height, title)
-        self.ball = Ball('мячик.png', 0.1)
-        self.ball.center_x = 300
-        self.ball.center_y = 300
-        self.ball.change_x = 2
+        self.ball = Ball(IMAGE_BALL, BALL_SCALE)
 
+    def setup(self):
+        self.ball.center_x = SCREEN_WIDTH/2
+        self.ball.center_y = SCREEN_HEIGHT/2
+        self.ball.change_x = SPEED_X
+        self.ball.change_y = SPEED_Y
     def on_draw(self):
         arcade.start_render()
         arcade.set_background_color((60, 179, 113))
@@ -20,8 +29,12 @@ class GameWindow(arcade.Window):
 class Ball(arcade.Sprite):
     def update(self):
         self.center_x += self.change_x
-        if self.right > 600 or self.left < 0:
+        if self.right > SCREEN_WIDTH or self.left < 0:
             self.change_x = -self.change_x
+        self.center_y += self.change_y
+        if self.top > SCREEN_HEIGHT or self.bottom < 0:
+            self.change_y = -self.change_y
 
-window = GameWindow(600, 600, 'Ping-Pong')
+window = GameWindow(SCREEN_WIDTH, SCREEN_HEIGHT, 'Ping-Pong')
+window.setup()
 arcade.run()
